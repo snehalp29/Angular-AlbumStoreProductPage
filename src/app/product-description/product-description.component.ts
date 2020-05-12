88413475e8c987b4eb312a6494e1adab9a07b5e3
snehalp29 <snehalpatkar@Gmail.com>
2020-05-12 13:43:52 -0400

    bug fix

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 
 @Component({
-selector:'product-description',
-templateUrl:'./product-description.html',
+selector:'app-product-description',
+templateUrl:'./product-description.component.html',
 styleUrls:['./product-description.component.css']
 })
 export class ProductDescription{
