d9a45e36421697614de0ff88bb0517ee96e45919
snehalp29 <snehalpatkar@Gmail.com>
2020-05-12 13:32:31 -0400

    part 1

---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+selector:'product-description',
+templateUrl:'./product-description.html',
+styleUrls:['./product-description.component.css']
+})
+export class ProductDescription{
+
+}
