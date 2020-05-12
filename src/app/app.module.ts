97dfbb0f3b74d16fac2ef3583869139277ac2925
snehalp29 <snehalpatkar@Gmail.com>
2020-05-12 14:54:11 -0400

    moved product service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import {ProductDescription} from './product-description/product-description.component';
-import { ProductService } from './product-service/product-service';
+import { ProductService } from './product-service';
 
 @NgModule({
   declarations: [
